ef0d4cb05656e2b3d51fb40ad94320b297e06df0
TaiDT <phuongdongdev777@gmail.com>
2025-04-19 23:57:32 +0700

    leaderboard

--- src/routes/api/userProgress.js
+++ src/routes/api/userProgress.js
@@ -7,6 +7,20 @@ const { authenticateUser } = require("~/middlewares/authMiddleware");
 // [GET] /Manga
 router.get("/all", UserProgressController.get);
 
+router.get("/leaderboard", authenticateUser, async (req, res, next) => {
+  try {
+    // có thể nhận ?limit=10 từ query string
+    const limit = parseInt(req.query.limit) || 10;
+    const data = await UserProgressController.getLeaderboard(limit);
+    return res.json({
+      code: 0,
+      message: "OK",
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 router.get("/getByUser", authenticateUser, UserProgressController.getByUser);
 
 // POST hoàn thành lesson:   /api/userProgress/complete
